e79704d3788e836c040487595196d12e3005d77f
Quinlan <quinnbush5@gmail.com>
2024-10-25 14:50:44 -0700

    progress push

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+import { withSentryConfig } from "@sentry/nextjs";
+
+const nextConfig = {
+  // Your existing Next.js configuration
+  experimental: {
+    instrumentationHook: true,
+  },
 };
 
-export default nextConfig;
+// Make sure adding Sentry options is the last code to run before exporting
+export default withSentryConfig(nextConfig, {
+  org: "project-xx",
+  project: "repairshop",
+
+  // An auth token is required for uploading source maps.
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+
+  silent: false, // Can be used to suppress logs
+
+  hideSourceMaps: true,
+
+  disableLogger: true,
+});
